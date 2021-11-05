5d2b90617b6e59893cfd7ab1bebaea063cf04778
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-06 00:18:36 +0700

    init

---
+++ src/main/java/com/devteam/APIAPP.java
@@ -0,0 +1,13 @@
+package com.devteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIAPP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APIAPP.class, args);
+    }
+
+}
