4e34d76618ec705a79b8d493a632c96537428e9c
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-08 18:49:27 +0700

    update

--- src/main/java/com/devteam/DigitalApplication.java
+++ src/main/java/com/devteam/DigitalApplication.java
@@ -1,11 +1,7 @@
 package com.devteam;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(scanBasePackages="com.devteam.*")
 public class DigitalApplication{
