72f6cb952405d3e4d999df86a22331d3daafc52d
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-08 13:26:45 +0700

    update

--- src/main/java/com/devteam/config/base/BaseEntity.java
+++ src/main/java/com/devteam/config/base/BaseEntity.java
@@ -6,6 +6,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,17 +24,21 @@ abstract  public class BaseEntity<T extends Serializable> {
     private static final long serialVersionUID = 1L;
 
     @Column(name = "created_by")
+    @CreatedBy
     private String createdBy;
 
     @JsonFormat(pattern = DateUtil.COMPACT_DATETIME_FORMAT)
     @Column(name = "created_time")
+    @CreatedDate
     private Date createdTime;
 
     @Column(name = "modified_by")
+    @LastModifiedBy
     private String modifiedBy;
 
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = DateUtil.COMPACT_DATETIME_FORMAT)
     @Column(name = "modified_time")
+    @LastModifiedDate
     private Date  modifiedTime;
 
 }
