b4384c99427104226c127f9ca94e1f692531bcdb
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-10 18:32:40 +0700

    update

---
+++ digital-app/dist/@types/index.d.ts
@@ -0,0 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+//# sourceMappingURL=index.d.ts.map
---
+++ src/main/resources/public/@types/App.d.ts
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+import './App.scss';
+export declare class App extends Component {
+    constructor(props: any);
+    render(): JSX.Element;
+}
+//# sourceMappingURL=App.d.ts.map
