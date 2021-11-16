306f7b606c43d3e556f10845231c6f476cf45da0
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-16 14:27:00 +0700

    update

--- src/main/java/com/devteam/security/WebResourceConfig.java
+++ src/main/java/com/devteam/security/WebResourceConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -76,13 +75,6 @@ public class WebResourceConfig extends WebMvcConfigurationSupport {
                 .addResourceHandler("/**")
                 //.addResourceLocations("classpath:/public/");
                 .addResourceLocations(publicPaths);
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry
-                .addViewController("/swagger-ui/")
-                .setViewName("forward:/swagger-ui/index.html");
     }
 }
 
