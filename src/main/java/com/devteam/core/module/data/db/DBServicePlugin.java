abee28b289a7cf39b52e06c1a0fcecf2524ae3a3
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-17 18:40:44 +0700

    update

--- src/main/java/com/devteam/core/module/data/db/DBServicePlugin.java
+++ src/main/java/com/devteam/core/module/data/db/DBServicePlugin.java
@@ -14,28 +14,10 @@ public class DBServicePlugin {
   
   final static public int CORE_SECURITY = 0;
   final static public int SETTINGS      = 1;
-
   final static public int ACCOUNT  = 10;
-
   final static public int COMPANY  = 20;
   final static public int COMPANY_HR  = 30;
-  final static public int COMMUNICATION = 40;
 
-  final static public int PARTNER  = 50;
-  final static public int PROJECT    = 60;
-
-  final static public int COMPANY_MISC  = 61;
-  
-  final static public int ACCOUNTING  = 70;
-  final static public int ACCOUNTING_HR  = 71;
-
-  
-  final static public int ASSET  = 80;
-  final static public int PRODUCT  = 90;
-
-
-  final static public int LEGACY  = 150;
-  
   final static public int APP  = 200;
 
   public void initDb(ClientInfo client, ApplicationContext context) throws Exception {
