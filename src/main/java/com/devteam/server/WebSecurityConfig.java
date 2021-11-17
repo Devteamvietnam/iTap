687680bcc77fcdc642b4309c108f0bf19d41e94c
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-17 18:11:44 +0700

    update

--- src/main/java/com/devteam/server/WebSecurityConfig.java
+++ src/main/java/com/devteam/server/WebSecurityConfig.java
@@ -30,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().ignoringAntMatchers("/h2-console/**")
                 .and()
                 .cors().disable();
+        httpSecurity.csrf().disable();
 
     }
 
