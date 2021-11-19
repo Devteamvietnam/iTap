9fcfe8b9a494fc2dfdb031b2936d0a37044e678e
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-19 18:39:18 +0700

    update

--- web/src/Views/App.tsx
+++ web/src/Views/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-export default class App extends React.Component {
+
+export class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      name: "Digital"
+    };
+    
+  }
+  handleChangeName(event: any) {
+    this.setState({ name: event.target.value });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <input type="text" value={this.state.name} onChange={this.handleChangeName.bind(this)} />
           <p>
-            Digital <code>VFX</code> Project
+            {this.state.name} <code>VFX</code> Project
           </p>
         </header>
       </div>
