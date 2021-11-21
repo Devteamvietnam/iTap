7da0cd8e11a8ba9d3ff1a658d68a392ed608e18b
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-21 15:02:59 +0700

    update

--- web-ui/src/index.tsx
+++ web-ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global.scss';
-import {App} from './Views/App';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
